c4d9ddeb09879552c9b28ab87fd87c7a2819929d
Ahmed El Sayyad <elsayyada197@gmail.com>
2026-06-11 15:35:38 -0700

    feat: implement custom TabBar navigation

--- AuraCast/Views/Navigation/TabBar.swift
+++ AuraCast/Views/Navigation/TabBar.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct TabBar: View {
+    
+    var action:()-> Void
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        ZStack{
+
+            HStack{
+                Button {
+                    action()
+                } label: {
+                    
+                    Image(systemName: "mappin.and.ellipse")
+                        .frame(width: 44, height: 44)
+                    
+                    Spacer()
+                    
+                    NavigationLink{
+                        
+                        
+                    } label:{
+                        Image(systemName: "list.star")
+                            .frame(width: 44, height: 44)
+                    }
+                }
+                
+            }
+            .font(.title3)
+            .foregroundColor(.white)
+            .padding(EdgeInsets(top: 20, leading: 32, bottom: 24, trailing: 32))
+        }
+        .frame(maxHeight:.infinity , alignment: .bottom)
+        .ignoresSafeArea()
     }
 }
 
 struct TabBar_Previews: PreviewProvider {
     static var previews: some View {
-        TabBar()
+        TabBar(action: {
+            print("Tab pressed!")
+        })
+            .preferredColorScheme(.dark)
     }
 }
