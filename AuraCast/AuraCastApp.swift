91c23d38cc0d42b8b34b2646a42bb5eece489364
Ahmed El Sayyad <elsayyada197@gmail.com>
2026-06-10 17:33:00 -0700

    Initial Commit

---
+++ AuraCast/AuraCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraCastApp.swift
+//  AuraCast
+//
+//  Created by Ahmed El Sayyad Mohamed on 10/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct AuraCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuraCast/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AuraCast
+//
+//  Created by Ahmed El Sayyad Mohamed on 10/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
